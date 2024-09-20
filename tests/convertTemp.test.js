8e1ff6435020c4cc5aa1901c938fb2f9b1b0b80c
Elmer Balbin <contact@elmer.dev>
2024-09-20 12:04:23 -0700

    updated test cases

--- tests/convertTemp.test.js
+++ tests/convertTemp.test.js
@@ -2,3 +2,7 @@
 // Test case 1: Converting 30 deg celsius will return 86 deg fahrenheit
 // Test case 2: Converting 104 deg fahrenheit will return 40 deg celsius
 // Both test cases need to pass
+
+const Converter = require('../src/convertTemp')
+
+const convert = new Converter()
--- tests/counter.test.js
+++ tests/counter.test.js
@@ -3,3 +3,5 @@
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
 // HINT: Use jest .resolves() method so that it is easier to work with async functions
+
+const counter = require('../src/counter')
