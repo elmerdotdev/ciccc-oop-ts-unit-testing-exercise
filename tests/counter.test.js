e7d53e21ba95f8b2e9520e662c336dc97a11e9aa
Elmer Balbin Jr. <contact@elmer.dev>
2024-09-20 12:53:58 -0700

    Update counter.test.js

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -2,6 +2,6 @@
 // Test case 1: counter(10, 'increment') will eventualy return 10
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
-// HINT: Use jest .resolves() method so that it is easier to work with async functions
+// HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
 const counter = require('../src/counter')
